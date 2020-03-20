75e07a1a9c8a6d11bc5551be78731e60d63984be
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-03-19 17:21:30 -0700

    signIn logic

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -49,7 +49,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyChlUfSwiqjrZk0ycUmzPVbKQI1VmbjNak',
         {
           email: email,
           password: password,
